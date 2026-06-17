8f3665d2faa44f19b24f3905918238d437e6e862
rush-limitless <arnaudsoutenance@gmail.com>
2026-06-17 23:47:12 +0100

    fix: phone mockup image overflow, use object-contain

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -29,7 +29,9 @@ function PhoneMockup({ src, alt }: { src: string; alt: string }) {
     <div className="flex items-center justify-center w-full h-full p-6 md:p-10">
       <div className="relative h-[90%] aspect-[9/19.5] bg-black rounded-[40px] p-[6px] shadow-2xl border-[3px] border-gray-800">
         <div className="absolute top-[10px] left-1/2 -translate-x-1/2 w-[80px] h-[22px] bg-black rounded-full z-10" />
-        <img src={src} alt={alt} className="w-full h-full object-cover rounded-[34px]" />
+        <div className="w-full h-full rounded-[34px] overflow-hidden bg-white">
+          <img src={src} alt={alt} className="w-full h-full object-contain" />
+        </div>
       </div>
     </div>
   )
